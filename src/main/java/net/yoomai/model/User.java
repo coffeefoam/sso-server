ef4198f7ffb8c54bbdf61bdd6d097d5639ebea82
RayYu <coffeefoam@126.com>
2012-09-12 23:28:48 +0800

    update

--- src/main/java/net/yoomai/model/User.java
+++ src/main/java/net/yoomai/model/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -18,7 +19,7 @@ import java.util.Date;
  * @since 1.5
  */
 @Entity
-public class User {
+public class User implements Serializable {
 	@Id
 	@GeneratedValue
 	private long uid;
