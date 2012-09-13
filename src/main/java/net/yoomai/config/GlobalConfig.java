12f1f027dd20564b5ac503196b0e6003b8aaa68b
Ray yu <coffeefoam@126.com>
2012-09-13 18:30:01 +0800

    commit

--- src/main/java/net/yoomai/config/GlobalConfig.java
+++ src/main/java/net/yoomai/config/GlobalConfig.java
@@ -4,8 +4,23 @@
  */
 package net.yoomai.config;
 
+import com.google.inject.name.Named;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
 /**
  * @(#)GlobalConfig.java 1.0 13/09/2012
+ * </p>
+ * 全局性配置文件的加载与设置。
  */
 public class GlobalConfig {
+  	private PropertiesConfiguration configuration;
+
+	public GlobalConfig(@Named("properties.name") String propName) {
+		try {
+			configuration = new PropertiesConfiguration(propName);
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
 }
