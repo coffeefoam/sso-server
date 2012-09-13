03807098fdb8c5b662f719a3bab89ac657091646
Ray yu <coffeefoam@126.com>
2012-09-13 18:14:43 +0800

    commit

---
+++ src/main/java/net/yoomai/config/GlobalConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2011, yoomai.net. All rights reserved.
+ * yoomai.cn. Use is subject to license terms.
+ */
+package net.yoomai.config;
+
+/**
+ * @(#)GlobalConfig.java 1.0 13/09/2012
+ */
+public class GlobalConfig {
+}
